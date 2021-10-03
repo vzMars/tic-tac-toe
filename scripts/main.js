4e2d74f4480c90443e80e99564f61f18f25e2874
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2021-10-03 19:49:10 -0400

    can add the player's marks to the gameboard

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,65 @@ const Player = (mark) => {
   const getMark = () => mark;
   return { getMark };
 };
+
+const playerX = Player('X');
+const playerO = Player('O');
+
+const displayController = (() => {
+  const _squares = Array.from(document.querySelectorAll('.square'));
+
+  document.getElementById('gameboard').addEventListener('click', (event) => {
+    console.log(event.target);
+    let index = _squares.findIndex((_square) => {
+      return _square === event.target;
+    });
+    gameBoard.addMark(index);
+    render();
+  });
+
+  const render = () => {
+    gameBoard.getBoard().forEach((mark, index) => {
+      _squares[index].textContent = mark;
+    });
+  };
+
+  return {
+    render,
+  };
+})();
+
+const gameBoard = (() => {
+  const _board = ['', '', '', '', '', '', '', '', ''];
+
+  const getBoard = () => _board;
+
+  const clearBoard = () => {
+    for (let i = 0; i < _board.length; i++) {
+      _board[i] = '';
+    }
+  };
+
+  const addMark = (index) => {
+    _board[index] = gameLogic.getCurrentTurn();
+    gameLogic.setCurrentTurn();
+    console.log(_board);
+  };
+
+  return { getBoard, clearBoard, addMark };
+})();
+
+const gameLogic = (() => {
+  let _currentTurn = playerX.getMark();
+
+  const getCurrentTurn = () => _currentTurn;
+
+  const setCurrentTurn = () => {
+    if (getCurrentTurn() === 'X') {
+      _currentTurn = playerO.getMark();
+    } else {
+      _currentTurn = playerX.getMark();
+    }
+  };
+
+  return { getCurrentTurn, setCurrentTurn };
+})();
