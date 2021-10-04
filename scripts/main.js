5e343a1a4e1884b77d31c6cae58699a7cf265c97
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2021-10-04 14:18:30 -0400

    game checks if the player can add a marker to the spot they clicked on and if the game is over and if it ended as a win or a draw

--- scripts/main.js
+++ scripts/main.js
@@ -8,15 +8,22 @@ const playerO = Player('O');
 
 const displayController = (() => {
   const _squares = Array.from(document.querySelectorAll('.square'));
+  const gameInfo = document.querySelector('.game-info');
 
   document.getElementById('gameboard').addEventListener('click', (event) => {
-    console.log(event.target);
     let index = _squares.findIndex((_square) => {
       return _square === event.target;
     });
-    gameBoard.addMark(index);
-    render();
+    if (!gameLogic.getGameOver()) {
+      gameBoard.checkIndex(index);
+      render();
+    }
   });
+
+  const changeGameInfo = (text) => {
+    gameInfo.textContent = text;
+    render();
+  };
 
   const render = () => {
     gameBoard.getBoard().forEach((mark, index) => {
@@ -25,42 +32,110 @@ const displayController = (() => {
   };
 
   return {
-    render,
+    changeGameInfo,
   };
 })();
 
 const gameBoard = (() => {
-  const _board = ['', '', '', '', '', '', '', '', ''];
+  const board = ['', '', '', '', '', '', '', '', ''];
 
-  const getBoard = () => _board;
+  const getBoard = () => board;
 
   const clearBoard = () => {
-    for (let i = 0; i < _board.length; i++) {
-      _board[i] = '';
+    for (let i = 0; i < board.length; i++) {
+      board[i] = '';
     }
   };
 
   const addMark = (index) => {
-    _board[index] = gameLogic.getCurrentTurn();
-    gameLogic.setCurrentTurn();
-    console.log(_board);
-  };
-
-  return { getBoard, clearBoard, addMark };
-})();
-
-const gameLogic = (() => {
-  let _currentTurn = playerX.getMark();
-
-  const getCurrentTurn = () => _currentTurn;
-
-  const setCurrentTurn = () => {
-    if (getCurrentTurn() === 'X') {
-      _currentTurn = playerO.getMark();
+    board[index] = gameLogic.getCurrentTurn();
+    if (gameLogic.checkWinner(gameLogic.getCurrentTurn())) {
+      displayController.changeGameInfo(
+        `Player ${gameLogic.getCurrentTurn()} has won!`
+      );
+      gameLogic.setGameOver();
     } else {
-      _currentTurn = playerX.getMark();
+      gameLogic.setCurrentTurn();
+      displayController.changeGameInfo(
+        `Player ${gameLogic.getCurrentTurn()}'s turn!`
+      );
+      gameLogic.checkDraw();
     }
   };
 
-  return { getCurrentTurn, setCurrentTurn };
+  const checkIndex = (index) => {
+    if (board[index] === '') {
+      addMark(index);
+    }
+  };
+
+  return { getBoard, clearBoard, checkIndex };
+})();
+
+const gameLogic = (() => {
+  let currentTurn = playerX.getMark();
+  let winner = false;
+  let gameOver = false;
+  const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  const getCurrentTurn = () => currentTurn;
+
+  const setCurrentTurn = () => {
+    if (getCurrentTurn() === 'X') {
+      currentTurn = playerO.getMark();
+    } else {
+      currentTurn = playerX.getMark();
+    }
+  };
+
+  const getGameOver = () => gameOver;
+
+  const setGameOver = () => {
+    if (getGameOver()) {
+      gameOver = false;
+    } else {
+      gameOver = true;
+    }
+  };
+
+  const checkWinner = (mark) => {
+    console.log(`checking if player${mark} is the winner`);
+    return (winner = winningCombos.some((combo) => {
+      console.log(`checking if ${combo} combo has ${mark} mark`);
+      return combo.every((index) => {
+        console.log(
+          `index: ${index}  hasMark: ${gameBoard
+            .getBoard()
+            [index].includes(mark)}`
+        );
+        return gameBoard.getBoard()[index].includes(mark);
+      });
+    }));
+  };
+
+  const checkDraw = () => {
+    let drawStatus = !gameBoard.getBoard().includes('');
+    if (drawStatus) {
+      displayController.changeGameInfo(`It's a draw!`);
+      setGameOver();
+    }
+  };
+
+  return {
+    getCurrentTurn,
+    setCurrentTurn,
+    getGameOver,
+    setGameOver,
+    checkWinner,
+    checkDraw,
+  };
 })();
